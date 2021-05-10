1a2e853f780e7fd46fd3e810baca472dbbd29212
Joseph <josephtsegen10@gmail.com>
2021-05-10 01:28:12 +0100

    decided to use typescript with react instead

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React, { Fragment } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Fragment>
+        <p>Using typescript...</p>
+      </Fragment>
+    </div>
+  );
+}
+
+export default App;
