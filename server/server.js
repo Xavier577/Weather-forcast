f8f0149ab5db506dbeb8945f0ecc4a3d199752e9
Tsegen <Paul.Tsegen@ng.kpmg.com>
2021-07-20 13:22:59 +0100

    nextjs for frontend

--- server/server.js
+++ server/server.js
@@ -1,20 +1,24 @@
 require("dotenv").config()
 
 const WEATHERSTACK_API = process.env.WEATHERSTACK_API
+const OPENWEATHER_API = process.env.OPENWEATHER_API
 const express = require("express")
 const axios = require("axios")
 const { response } = require("express")
 
 const app = express()
 const PORT = process.env.PORT || 8080
-const API_URL = `http://api.weatherstack.com/current?access_key=${WEATHERSTACK_API}&query=New%York`
+const API_URL = `https://api.openweathermap.org/data/2.5/weather?q=london&appid=${OPENWEATHER_API}`//`http://api.weatherstack.com/current?access_key=${WEATHERSTACK_API}&query=New%York`
 
 
 
-app.get("/", (req, res) => {
-    axios.get(API_URL).then(data => {
-        res.send(data.data)
-    }).catch(error => console.log(error))
+app.post("/forcast", (req, res) => {
+    // this is to get data by geographic coordinates
+   // const url = `https://api.openweathermap.org/data/2.5/weather?lat=${req.body.lat}&lon=${req.body.lon}&appid=${OPENWEATHER_API}`
+    axios({
+        url: API_URL,
+        responseType: 'json'
+    }).then(data => res.json(data.data))
   
 })
 
