08ee47e6af43d139f0be5c163c126f8caf8f66b6
joseph <josephtsegen10@gmail.com>
2021-09-19 18:29:51 +0100

    post final fixes

--- server/src/routes/forcast.ts
+++ server/src/routes/forcast.ts
@@ -5,7 +5,7 @@ const forcastRouter = fileRouter();
 
 forcastRouter.post("/", (request, response) => {
   const { latitude, longitude } = request.body;
-  if (longitude && latitude) {
+  if (longitude && latitude && OPENWEATHER_API_ID) {
     const queryUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${OPENWEATHER_API_ID}`;
     axios
       .get(queryUrl)
@@ -13,7 +13,9 @@ forcastRouter.post("/", (request, response) => {
       .catch(({ errno, code, syscall, isAxiosError, protocol, host }) =>
         response.send({ errno, code, syscall, isAxiosError, protocol, host })
       );
-  }
+  } else if (!OPENWEATHER_API_ID) {
+    response.send({ errMessage: "Missing Api Key" });
+  } else response.send({ errMessage: "Server Error" });
 });
 
 export default forcastRouter;
--- server/src/routes/locationSearchQuery.ts
+++ server/src/routes/locationSearchQuery.ts
@@ -5,8 +5,8 @@ const locationSearchQueryRouter = fileRouter();
 
 locationSearchQueryRouter.post("/", (request, response) => {
   const { searchQuery } = request.body;
-  const geoQueryUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${searchQuery}&limit=5&appid=${OPENWEATHER_API_ID}`;
-  if (searchQuery) {
+  if (searchQuery && OPENWEATHER_API_ID) {
+    const geoQueryUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${searchQuery}&limit=5&appid=${OPENWEATHER_API_ID}`;
     axios
       .get(geoQueryUrl)
       .then((apiResponse) => {
@@ -15,6 +15,10 @@ locationSearchQueryRouter.post("/", (request, response) => {
       .catch(({ errno, code, syscall, isAxiosError, protocol, host }) =>
         response.send({ errno, code, syscall, isAxiosError, protocol, host })
       );
+  } else if (!OPENWEATHER_API_ID) {
+    response.send({ errMessage: "Missing Api Key" });
+  } else {
+    response.send({ errMessage: "Server Error" });
   }
 });
 
